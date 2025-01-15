6d074feef79252800a49782944e9fdd8b472d3eb
lizzct82 <lizzct@gmail.com>
2025-01-15 10:08:36 +0800

    Initial commit: Flask ML app

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def predict():
     data = request.json
     features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)
-    return jsonify({'prediction': int(prediction[0])})
+    return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000
